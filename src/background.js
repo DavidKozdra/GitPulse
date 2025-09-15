86af0a2f8a966e3604dc0c2b72107b1b8c908de2
David Kozdra <davidkozdra@gmail.com>
2025-09-15 13:46:12 -0400

    base extention content

---
+++ src/background.js
@@ -0,0 +1,18 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Extension Installed");
+  });
+  
+  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "open-url") {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        if (!tabs || !tabs.length || !tabs[0].url) {
+          alert("Cannot access the current tab URL. Make sure you're on a regular webpage.");
+          return;
+        }
+      
+       
+      });
+      
+    }
+  });
+  
